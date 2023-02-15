a974a304de2a0fde2dc655fc66514fe0a60f2975
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-02-15 15:15:54 -0500

    Add Basic Services logic + Rename interfaces to make them easier to detect.

---
+++ src/services/offers.ts
@@ -0,0 +1,11 @@
+import { BaseService, IClientInitOptions } from '../client.js';
+
+export class OfferService extends BaseService {
+  constructor(options: IClientInitOptions) {
+    super(options);
+  }
+
+  public GetAll = async () => {};
+
+  public GetById = async () => {};
+}
