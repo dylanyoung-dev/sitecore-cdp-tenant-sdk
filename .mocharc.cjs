14618cdac62dbc0b702c4ea918cee85a6a85ba44
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-02-16 11:56:51 -0500

    Added initial unit testing framework

---
+++ .mocharc.cjs
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  recursive: true,
+  timeout: 3000,
+  spec: ['./dist/**/*.spec.js'],
+};
---
+++ src/services/decisionModels.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+
+describe('Decision Models Service', () => {
+  // let service: DecisionModelsService;
+  // beforeEach(() => {
+  //     TestBed.configureTestingModule({});
+  //     service = TestBed.inject(DecisionModelsService);
+  // });
+  // it('should be created', () => {
+  //     expect(service).toBeTruthy();
+  // });
+});
