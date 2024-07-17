8edfef49f8784f233dcd0657b65cda8e7b43ef7b
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2024-07-17 10:24:34 -0400

    Added Unit Testing

--- src/client.spec.ts
+++ src/client.spec.ts
@@ -1,16 +1,25 @@
 import { expect } from 'chai';
-import { Client } from './client.js';
+import { Client, IClientInitOptions, RegionOptions } from './client.js';
 
 describe('Client', () => {
   let client: Client;
 
   beforeEach(() => {
     // Initialize the client instance before each test
-    //client = new Client();
+    client = new Client({
+      clientId: 'clientId',
+      clientSecret: 'clientSecret',
+      region: RegionOptions.US,
+    } as IClientInitOptions);
   });
 
   it('should be defined', () => {
     // Test if the client instance is defined
     expect(client).to.be.an.instanceOf(Client);
+  });
+
+  it('Check if correct region url is set for client', () => {
+    // Test if the client instance has the correct region set
+    expect(client.options.region).to.equal('api-engage-us.sitecorecloud.io');
   });
 });
